96ab81730fae07b344041dec34666ea70fbfed02
patrickgue <me@patrickguenthard.ch>
2020-03-25 18:19:41 +0100

    removed custom font

--- AudioRecorder/AudioMeter.swift
+++ AudioRecorder/AudioMeter.swift
@@ -11,27 +11,19 @@ import Cocoa
 
 class AudioMeter: NSView{
     var decibel:Float = -120
+    var centerX:Int = 170
+    var needlePositionY:Int = 10
     
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         
         let context = NSGraphicsContext.current?.cgContext;
         
-        context!.beginPath()
-        context!.move(to: CGPoint(x:170,y:10))
-        let needleEnd = position(decibel: decibel, radius: 120)
-        context!.addLine(to: CGPoint(x: 170 + needleEnd.x, y: 10 + needleEnd.y))
-        context!.setStrokeColor(CGColor(red: 1,green: 0,blue: 0,alpha: 1))
-        context!.setLineWidth(1.0)
-        context!.strokePath()
-        
-        
-        drawText(text: "dB",x: 167, y: 140, fontName: "AkzidenzGroteskBQ-Medium", fontSize: 10, color: NSColor.textColor)
+        drawText(text: "dB",x: 167, y: 140, fontSize: 10, color: NSColor.textColor)
         
         for dec in stride(from: -120, to: 1, by: 30) {
             let labelPosition = position(decibel: Float(dec), radius: 115)
-            
-            drawText(text: "\(dec)", x: 162 + labelPosition.x, y: 10+labelPosition.y, fontName: "AkzidenzGroteskBQ-Reg", fontSize: 10, color: NSColor.textColor)
+            drawText(text: "\(dec)", x: 162 + labelPosition.x, y: 10+labelPosition.y, fontSize: 10, color: NSColor.textColor)
         }
         
         
@@ -39,14 +31,22 @@ class AudioMeter: NSView{
             let needleStart = position(decibel: Float(dec), radius: 100)
             let needleEnd = position(decibel: Float(dec), radius: 90)
             context!.beginPath()
-            context!.move(to: CGPoint(x: 170 + needleStart.x, y: 10 + needleStart.y))
-            context!.addLine(to: CGPoint(x: 170 + needleEnd.x, y: 10 + needleEnd.y))
+            context!.move(to: CGPoint(x: centerX + needleStart.x, y: needlePositionY + needleStart.y))
+            context!.addLine(to: CGPoint(x: centerX + needleEnd.x, y: needlePositionY + needleEnd.y))
             context!.setStrokeColor(NSColor.textColor.cgColor)
             
             context!.setLineWidth(1.5)
             context!.strokePath()
             
         }
+        
+        context!.beginPath()
+        context!.move(to: CGPoint(x:centerX,y:10))
+        let needleEnd = position(decibel: decibel, radius: 120)
+        context!.addLine(to: CGPoint(x: centerX + needleEnd.x, y: needlePositionY + needleEnd.y))
+        context!.setStrokeColor(CGColor(red: 1,green: 0,blue: 0,alpha: 1))
+        context!.setLineWidth(1.0)
+        context!.strokePath()
         
     }
     
@@ -57,9 +57,9 @@ class AudioMeter: NSView{
         )
     }
     
-    func drawText(text:String, x:Int, y:Int, fontName:String, fontSize:Int,color:NSColor) {
+    func drawText(text:String, x:Int, y:Int, fontSize:Int,color:NSColor) {
         let atts = [
-            NSAttributedString.Key.font: NSFont.init(name: fontName, size: CGFloat(fontSize)),
+            NSAttributedString.Key.font: NSFont.systemFont(ofSize: CGFloat(fontSize)),
             NSAttributedString.Key.foregroundColor: color
         ]
         (text as NSString).draw(
